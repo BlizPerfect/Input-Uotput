2281ce234ea301860b7d9b0bd9fedd6cdec9cc0d
BlizPerfect <zax_2000@mail.ru>
2021-09-11 15:20:27 +0500

    Реализовал базовый функционал
    1. Реализовано суммирование значений.
    2. Реализовано нахождение разности чисел.
    3. Реализовано среднее арифметическое их модулей

---
+++ MyFirstGitApp/MyFirstGitApp/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace MyFirstGitApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите два числа:");
+            int firstNumber = 0;
+            int secondNumber = 0;
+            bool test = true;
+
+            while (test)
+            {
+                try
+                {
+                    firstNumber = Convert.ToInt32(Console.ReadLine());
+                    secondNumber = Convert.ToInt32(Console.ReadLine());
+                    test = false;
+                }
+                catch
+                {
+                    Console.WriteLine("Вы ввели не число, попробуйте сного ввести оба числа!");
+                    firstNumber = 0;
+                    secondNumber = 0;
+                }
+            }
+            Summ(firstNumber, secondNumber);
+            Diff(firstNumber, secondNumber);
+            Mod(firstNumber, secondNumber);
+        }
+        public static void Summ(double firstNumber, double secondNumber)
+        {
+            double result = firstNumber + secondNumber;
+            Console.WriteLine("Сумма этих числел: " + result);
+        }
+
+        public static void Diff(double firstNumber, double secondNumber)
+        {
+            double result = firstNumber - secondNumber;
+            Console.WriteLine("Разность этих числел: " + result);
+        }
+
+        public static void Mod(double firstNumber, double secondNumber)
+        {
+            double result = ((double)Math.Abs(firstNumber) + (double)Math.Abs(secondNumber)) / 2;
+            Console.WriteLine("Среднее арифметическое их модулей: " + result);
+        }
+    }
+}
